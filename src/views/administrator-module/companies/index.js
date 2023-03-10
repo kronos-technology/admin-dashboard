9fa7f4eead553a775a714a86f8e16e8b0ecb185a
Fredy Gonzalez <fredylingling.fgg6@gmail.com>
2023-03-10 10:16:38 -0500

    acomodando el index de companies

--- src/views/administrator-module/companies/index.js
+++ src/views/administrator-module/companies/index.js
@@ -1,138 +1,168 @@
-import React, { useEffect, useCallback } from 'react'
-import { AdaptableCard } from 'components/shared'
-import AdminTable from "../components/AdminTable";
-import { injectReducer } from 'store/index'
-import reducer from './store'
-import useThemeClass from 'utils/hooks/useThemeClass'
-import { useDispatch, useSelector } from 'react-redux'
-import { getCompanies } from './store/dataSlice'
-import { Avatar } from 'components/ui'
+import React, { useEffect, useCallback, useMemo } from 'react';
+import { AdaptableCard } from 'components/shared';
+import AdminTable from '../components/AdminTable';
+import { injectReducer } from 'store/index';
+import reducer from './store';
+import useThemeClass from 'utils/hooks/useThemeClass';
+import { useDispatch, useSelector } from 'react-redux';
+import { getCompanies, setTableData } from './store/dataSlice';
+import {
+  setSelectedCompanies,
+  toggleDeleteConfirmation,
+  setSortedColumn
+} from './store/stateSlice';
+import { Avatar } from 'components/ui';
+import { FiUser } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
+import { HiOutlinePencil, HiOutlineTrash } from 'react-icons/hi';
+import cloneDeep from 'lodash/cloneDeep';
+import { useNavigate } from 'react-router-dom';
 
+injectReducer('companies', reducer);
 
-injectReducer('companies', reducer)
+const ActionColumn = ({ row }) => {
+  const dispatch = useDispatch();
+  const { textTheme } = useThemeClass();
+  const navigate = useNavigate();
 
+  const onEdit = () => {
+    navigate(`/administrator/companies-edit/${row.id}`);
+  };
+  const onDelete = () => {
+    dispatch(toggleDeleteConfirmation(true));
+    dispatch(setSelectedCompanies(row.id));
+  };
 
+  return (
+    <div className="flex justify-end text-lg">
+      <span className={`cursor-pointer p-2 hover:${textTheme}`} onClick={onEdit}>
+        <HiOutlinePencil />
+      </span>
+      <span className="cursor-pointer p-2 hover:text-red-500" onClick={onDelete}>
+        <HiOutlineTrash />
+      </span>
+    </div>
+  );
+};
 
-const ActionColumn = ({row}) => {
-	
-	const { textTheme } = useThemeClass()
-	const dispatch = useDispatch()
+const NameColumn = ({ row }) => {
+  const { textTheme } = useThemeClass();
+  const avatar = row.img ? <Avatar src={row.img} /> : <Avatar icon={<FiUser />} />;
+  return (
+    <div className="flex items-center">
+      {avatar}
+      <span className={`ml-2 rtl:mr-2 font-semibold`}>{row.name}</span>
+    </div>
+  );
+};
 
-	const onEdit = () => {
-		// eslint-disable-next-line no-undef
-		dispatch(setDrawerOpen())
-		// dispatch(setSelectedCustomer(row))
-	}
-
-	return (
-		<div 
-			className={`${textTheme} cursor-pointer select-none font-semibold`}
-			onClick={onEdit}
-		>
-			Edit
-		</div>
-	)
-}
-
-const NameColumn = ({row}) => {
-
-	const { textTheme } = useThemeClass()
-
-	return (
-		<div className="flex items-center">
-			<Avatar size={28} shape="circle" src={row.img} />
-			<Link 
-				className={`hover:${textTheme} ml-2 rtl:mr-2 font-semibold`}
-				to={`/app/crm/customer-details?id=${row.id}`}
-			>
-				{row.name}
-			</Link>
-		</div>
-	)
-}
-
-let columns = [
-	{
-		Header: 'alias',
-		accessor: 'alias',
-		sortable: true,
-		Cell: props => {
-			const row = props.row.original
-			return <NameColumn row={row} />
-		},
-	},
-	{
-		Header: 'ID',
-		accessor: 'companyId',
-		// sortable: true,
-	},
-	{
-		Header: 'Name',
-		accessor: 'name',
-		// sortable: true,
-	},
-	{
-		Header: 'Phone',
-		accessor:'phone',
-		// soportable: true,
-	},
-	{
-		Header: 'Email',
-		accessor:'email',
-		// soportable: true,
-	},
-	{
-		Header: 'City',
-		accessor:'city',
-		// soportable: true,
-	},
-	{
-		Header: 'Address',
-		accessor:'address',
-		// soportable: true,
-	},
-	{
-		Header: 'Nit',
-		accessor:'nit',
-		// soportable: true,
-	},
-		{
-		Header: '',
-		id: 'action',
-		accessor: (row) => row,
-		Cell: props => <ActionColumn row={props.row.original} />
-	},
-]
-
-  
 const Companies = () => {
-	
-	const dispatch = useDispatch()
-	const data = useSelector((state) => state.companies.data.companiesList)
-	
-	const filterData = useSelector((state) => state.companies.data.filterData)
-	const { pageIndex, pageSize, sort, query } = useSelector((state) => 
-	state.companies.data.tableData)
-	
-	const fetchData = useCallback(() => {
-        dispatch(getCompanies({pageIndex, pageSize, sort, query, filterData}))
-	}, [pageIndex, pageSize, sort, query, filterData, dispatch])
+  const dispatch = useDispatch();
+  const { pageIndex, pageSize, sort, query, total } = useSelector(
+    (state) => state.companies.data.tableData
+  );
+  const loading = useSelector((state) => state.companies.data.loading);
 
-	useEffect(() => {
-		fetchData()
-	}, [fetchData, pageIndex, pageSize, sort, filterData])
-	
-	
-	
-	
-    return (
-        <>
-            <AdaptableCard className="h-full" bodyClass="h-full">
-            <AdminTable columns={columns} data={data}/>
-            </AdaptableCard>
-        </>
-    )
+  const data = useSelector((state) => state.companies.data.companiesList);
+
+  useEffect(() => {
+    fetchData();
+  }, [pageIndex, pageSize, sort]);
+
+  const tableData = useMemo(
+    () => ({ pageIndex, pageSize, sort, query, total }),
+    [pageIndex, pageSize, sort, query, total]
+  );
+  const fetchData = () => {
+    dispatch(getCompanies({ pageIndex, pageSize, sort, query }));
+  };
+  const columns = useMemo(() => [
+    {
+      Header: 'Name',
+      accessor: 'name',
+      sortable: true,
+      Cell: (props) => {
+        const row = props.row.original;
+        return <NameColumn row={row} />;
+      }
+    },
+    {
+      Header: 'CompanyId',
+      accessor: 'companyId',
+      sortable: true
+    },
+
+    {
+      Header: 'Name',
+      accessor: 'name',
+      sortable: true
+    },
+    {
+      Header: 'Phone',
+      accessor: 'phone'
+    },
+    {
+      Header: 'Email',
+      accessor: 'email'
+    },
+    {
+      Header: 'City',
+      accessor: 'city'
+    },
+    {
+      Header: 'Address',
+      accessor: 'address'
+    },
+    {
+      Header: 'Nit',
+      accessor: 'nit'
+    },
+
+    {
+      Header: '',
+      id: 'action',
+      accessor: (row) => row,
+      Cell: (props) => <ActionColumn row={props.row.original} />
     }
+  ]);
 
-export default Companies
+  const onPaginationChange = (page) => {
+    const newTableData = cloneDeep(tableData);
+    newTableData.pageIndex = page;
+    dispatch(setTableData(newTableData));
+  };
+  const onSelectChange = (value) => {
+    const newTableData = cloneDeep(tableData);
+    newTableData.pageSize = Number(value);
+    newTableData.pageIndex = 1;
+    dispatch(setTableData(newTableData));
+  };
+
+  const onSort = (sort, sortingColumn) => {
+    const newTableData = cloneDeep(tableData);
+    newTableData.sort = sort;
+    dispatch(setTableData(newTableData));
+    dispatch(setSortedColumn(sortingColumn));
+  };
+  return (
+    <>
+      <AdaptableCard className="h-full" bodyClass="h-full">
+        <AdminTable
+          title="Companies List"
+          entity="company"
+          columns={columns}
+          data={data}
+          skeletonAvatarColumns={[0]}
+          skeletonAvatarProps={{ className: 'rounded-md' }}
+          loading={loading}
+          pagingData={tableData}
+          onPaginationChange={onPaginationChange}
+          onSelectChange={onSelectChange}
+          onSort={onSort}
+        />
+      </AdaptableCard>
+    </>
+  );
+};
+
+export default Companies;
